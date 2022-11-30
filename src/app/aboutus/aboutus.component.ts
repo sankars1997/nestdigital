8bae870cc5aaaeb96869237581e0b5e81a0e6d4a
sankar <sankarsuresh1997@gmail.com>
2022-11-30 14:48:56 +0530

    homepage

---
+++ src/app/aboutus/aboutus.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutus',
+  templateUrl: './aboutus.component.html',
+  styleUrls: ['./aboutus.component.css']
+})
+export class AboutusComponent {
+data3:any=[
+  {
+    "head1":"MR. N. JEHANGIR  Mr. N. Jehangir, our Vice-Chairman and the Group Managing Director, is an aggressive entrepreneur who has set up 30 diversified enterprises catering to the global market in the last 25 years. He brings to NeST, his rich experience in successfully setting up and managing state of the art Hi tech industry in Kerala. He has won many laurels for his entrepreneurial achievements, such as - Best Regional Export Award, Best Entrepreneurship award, Prime Minister’s award for Technology Upgradation and Udyog Ratna award from the government of India for contribution in the field of Industrial development of the country, Top Exporter award from Technopark, Trivandrum. He works closely with the government of Kerala, KSIDC etc for the development of industries in Kerala and promoting Kerala abroad as an investment destination",
+  },
+  {
+    "head2":"rom an Engineer in semi-conductor development and manufacturing to the post of head semi-conductor development operations in 10 years time at IBM, heading over 2000 engineers and scientists – Mr. Javad Hassan’s excellence had paved for a spectacular growth in his career which started in early 1968. During this period he obtained over 20 patents for his inventions in semi-conductor technology. In later years, he held the positions of the corporate head of engineering and Technology at IBM’s world headquarters in New York as well as the head of their storage business, a 6 Billion Dollar revenue business. In 1988, he joined AMP Inc, world’s largest electronic components/connector company (a fortune 100 company), as their corporate head of technology. In his 10 years time, he diversified AMP into fiber-optics and wireless technologies, and built it to a 1.5+ billion Dollar business. He retired from AMP as the President of Global Inter Connect Systems business in 1998 and started off his career as entrepreneur in his own right. As Chairman, he has been the visionary guiding and managing the explosive growth of the Nest Group since its inception",
+  }
+]
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactusComponent,
+    AboutusComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
